9008bdbca90570e79a707e361590ded32ff023e4
Vuckovic, Luka (ITCDCC) - KLM <Luka.Vuckovic@klm.com>
2017-11-01 11:03:31 +0100

    Added season

--- src/main/java/com/example/southpark/graphqlsouthparkdemo/graphql/Query.java
+++ src/main/java/com/example/southpark/graphqlsouthparkdemo/graphql/Query.java
@@ -2,7 +2,9 @@ package com.example.southpark.graphqlsouthparkdemo.graphql;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.example.southpark.graphqlsouthparkdemo.model.Character;
+import com.example.southpark.graphqlsouthparkdemo.model.Season;
 import com.example.southpark.graphqlsouthparkdemo.service.CharacterService;
+import com.example.southpark.graphqlsouthparkdemo.service.SeasonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,5 +22,16 @@ public class Query implements GraphQLQueryResolver {
 
     public Character character(String name) {
         return characterService.getCharactersWithName(name);
+    }
+
+    @Autowired
+    SeasonService seasonService;
+
+    public List<Season> seasons() {
+        return seasonService.getAllSeasons();
+    }
+
+    public Season season(String name) {
+        return seasonService.getSeasonWithName(name);
     }
 }
